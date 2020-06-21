945ee8912fe9aa170f4463ef4b6cd0c05cb6f6b4
saichandrareddy1 <vsaichandrareddy@gmail.com>
2020-06-21 14:57:13 +0530

    Updated all

--- app.py
+++ app.py
@@ -62,31 +62,31 @@ def predict2():
     return render_template('index.html',
     prediction_text_Algo2=f' patient was {output}')
 
-@app.route('/predict3',methods=['POST'])
-def predict3():
+# @app.route('/predict3',methods=['POST'])
+# def predict3():
 
-    feature1 = request.form['Feature1_Algo3']
-    feature2 = request.form['Feature2_Algo3']
-    feature3 = request.form['Feature3_Algo3']
-    feature4 = request.form['Feature4_Algo3']
+#     feature1 = request.form['Feature1_Algo3']
+#     feature2 = request.form['Feature2_Algo3']
+#     feature3 = request.form['Feature3_Algo3']
+#     feature4 = request.form['Feature4_Algo3']
 
-    li = [feature1, feature2, feature3, feature4]
+#     li = [feature1, feature2, feature3, feature4]
 
-    integer_ = [int(i) for i in li]
-    final_features = [np.array(integer_)]
-    predict_value = model.predict(final_features)
+#     integer_ = [int(i) for i in li]
+#     final_features = [np.array(integer_)]
+#     predict_value = model.predict(final_features)
 
-    if predict_value[0] == 0:
-        output = "normal find"
-    elif predict_value[0] == 1:
-        output = "metastases"
-    elif predict_value[0] == 2:
-        output = "malign lymph"
-    else:
-        output = "fibrosis"
+#     if predict_value[0] == 0:
+#         output = "normal find"
+#     elif predict_value[0] == 1:
+#         output = "metastases"
+#     elif predict_value[0] == 2:
+#         output = "malign lymph"
+#     else:
+#         output = "fibrosis"
 
-    return render_template('index.html',
-    prediction_text_Algo3=f' patient was {output}')
+#     return render_template('index.html',
+#     prediction_text_Algo3=f' patient was {output}')
 
 
 
